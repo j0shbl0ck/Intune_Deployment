2012d40d7494d2f1c94cd1446547d3e13e210ba7
Joshua Block <70075839+j0shbl0ck@users.noreply.github.com>
2021-12-22 10:41:18 -0600

    Update install.ps1

--- BGInfo64/install.ps1
+++ BGInfo64/install.ps1
@@ -9,11 +9,11 @@ Description: Copies the BGInfo64.exe and custom.bgi onto device and runs on logo
 #>
 
 
-New-Item �ItemType Directory �Force �Path "C:\Program Files\BGInfo" | Out-Null
-Copy-Item �Path "$PSScriptRoot\Bginfo64.exe" �Destination "C:\Program Files\BGInfo\BGInfo64.exe"
-Copy-Item �Path "$PSScriptRoot\Workstations.bgi" �Destination "C:\Program Files\BGInfo\Workstations.bgi"
+New-Item -ItemType Directory -Force -Path "C:\Program Files\BGInfo" | Out-Null
+Copy-Item -Path "$PSScriptRoot\Bginfo64.exe" -Destination "C:\Program Files\BGInfo\BGInfo64.exe"
+Copy-Item -Path "$PSScriptRoot\Workstations.bgi" -Destination "C:\Program Files\BGInfo\Workstations.bgi"
 
-$Shell = New-Object �ComObject ("WScript.Shell")
+$Shell = New-Object -ComObject ("WScript.Shell")
 $ShortCut = $Shell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\BGInfo.lnk")
 $ShortCut.TargetPath="`"C:\Program Files\BGInfo\BGInfo64.exe`""
 $ShortCut.Arguments="`"C:\Program Files\BGInfo\Workstations.bgi`" /timer:0 /silent /nolicprompt"

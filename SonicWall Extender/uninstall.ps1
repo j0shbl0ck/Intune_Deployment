41319a3ca00e005a8c1ac88a5470c30a3567850a
Joshua Block <70075839+j0shbl0ck@users.noreply.github.com>
2021-12-22 13:29:08 -0600

    Update uninstall.ps1

--- SonicWall Extender/uninstall.ps1
+++ SonicWall Extender/uninstall.ps1
@@ -7,6 +7,6 @@ Unistall SonicWallNetExtender without restart.
 
 #>
 
-#Removes NetExtender by Product code
+#Removes NetExtender by Product code. Note, registry files will still be present. Remove /norestart for complete removal.
 msiexec /x “{EF06A6A8-6B81-4A09-8223-789953972FFF}” /q /norestart
 

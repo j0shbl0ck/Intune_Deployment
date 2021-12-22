f862afa568369bc33fd1ed8d250f5a4e1e4c408a
Joshua Block <70075839+j0shbl0ck@users.noreply.github.com>
2021-12-22 13:28:27 -0600

    Update install.ps1

--- SonicWall Extender/install.ps1
+++ SonicWall Extender/install.ps1
@@ -14,7 +14,7 @@ Copy-Item –Path "$PSScriptRoot\NetExtender-10.2.315.msi" –Destination "C:\MD
 Copy-Item –Path "$PSScriptRoot\profile.ps1" –Destination "C:\MDM\SonicWallNetExtender\profile.ps1"
 Copy-Item –Path "$PSScriptRoot\Create_Profile.bat" –Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\Create_Profile.bat"
 
-#This installs SonicWall NetExtender.
+#This installs SonicWall NetExtender.Note, this does a restart! If you perform /norestart the file will not install correct.
 msiexec /i "C:\MDM\SonicWallNetExtender\NetExtender-10.2.315.msi" /q 
 
 

d544cd14ee4901b57b8916689e3fa4078f597383
Joshua Block <70075839+j0shbl0ck@users.noreply.github.com>
2021-12-22 13:30:39 -0600

    Update Find_GUID_EXE_MSI.ps1

--- PowerShell scripts/Find_GUID_EXE_MSI.ps1
+++ PowerShell scripts/Find_GUID_EXE_MSI.ps1
@@ -1,3 +1,5 @@
-﻿get-wmiobject Win32_Product | Sort-Object -Property Name | Format-Table IdentifyingNumber, Name, LocalPackage -AutoSize
+## This fetches the product ID of applications for removing products via MSI if available.
+
+get-wmiobject Win32_Product | Sort-Object -Property Name | Format-Table IdentifyingNumber, Name, LocalPackage -AutoSize
 
 Pause

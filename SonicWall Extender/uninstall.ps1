af5a36ea70c91fa164da62d1588b4e81555451fd
Joshua Block <70075839+j0shbl0ck@users.noreply.github.com>
2021-12-22 10:29:50 -0600

    Add files via upload

---
+++ SonicWall Extender/uninstall.ps1
@@ -0,0 +1,12 @@
+﻿<#
+Author: Josh Block (jblock@trustinfinitech.com)
+Date: 12.21.21
+
+Description:
+Unistall SonicWallNetExtender without restart. 
+
+#>
+
+#Removes NetExtender by Product code
+msiexec /x “{EF06A6A8-6B81-4A09-8223-789953972FFF}” /q /norestart
+

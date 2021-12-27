76d7881b112dc0ef75c401d5be77aa05971f6b42
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2021-12-27 08:00:51 -0600

    Uploaded new powershell file

---
+++ Hide_File_Folder.ps1
@@ -0,0 +1,15 @@
+﻿<#
+Author: J Block
+Type: Public
+Date: 12.21.21
+Source: https://www.majorgeeks.com/content/page/how_to_hide_files_or_folders_using_command_prompt_or_powershell.html
+Description: This script is made to hide folders and files from end user.
+#>
+
+## This hides folders and files even with "view hidden folders on"
+attrib +s +h "C:\Users\Public\Desktop\Google Chrome.lnk"
+
+attrib +s +h "C:\Users\Public\Desktop\Firefox.lnk"
+
+## This unhides folders and files
+#attrib -s -h C:\Uasers\IntuneTest\Desktop\Google Chrome.lnk"

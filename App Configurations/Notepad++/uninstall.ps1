574e6304ab7d89159dddbddfe2f8652fd0515afc
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2021-12-29 13:41:22 -0600

    Uploaded uninstall.ps1

--- App Configurations/Notepad++/uninstall.ps1
+++ App Configurations/Notepad++/uninstall.ps1
@@ -12,3 +12,6 @@
 
 # Removes Notepad++ through uninstall.exe provided in source files. 
 start-process -FilePath "C:\Program Files\Notepad++\uninstall.exe" -ArgumentList '/S' -Verb runas -Wait
+
+# Removes remaing files and folder created by Notepad++
+Remove-Item -Path "C:\Program Files\Notepad++" -Recurse -Force -Confirm:$false

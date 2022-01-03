4c8a809f2f2624e769847b5ee89f376388d9a660
Josh Block <247jblock@gmail.com>
2022-01-03 15:53:42 -0600

    Added version to batch file

--- PowerShell scripts/Device Rename/remove_files.bat
+++ PowerShell scripts/Device Rename/remove_files.bat
@@ -1,9 +1,12 @@
+:: Version 1.0.2
 :: This batch file excutes on startup to remove the folders and files below. In a sense, it removes the folder, then self distructs itself. 
 
 CONSOLESTATE /Hide 
 
 :: This removes the DeviceRename folder created.
-del "C:\MDM\DeviceRename"
+del "C:\MDM\DeviceRename" /Q
+
+echo Y
 
 :: This removes the batch file copied into the Startup folder. 
 del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\remove_files.bat"

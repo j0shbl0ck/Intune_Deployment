3803e892d4acff3eaf5018963ddd699dad49ec44
Josh Block <247jblock@gmail.com>
2022-01-03 13:42:59 -0600

    Updated installer file path

--- App Configurations/Notepad++/install.ps1
+++ App Configurations/Notepad++/install.ps1
@@ -2,7 +2,7 @@
     .NOTES
     =============================================================================
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.1
+    Version: 1.0.2
     Date: 12.29.21
     Type: Public
     Source: https://stackoverflow.com/questions/50274909/unattended-silent-install-notepad
@@ -14,7 +14,7 @@
 New-Item -ItemType Directory -Force -Path "C:\MDM\NP+" | Out-Null
 
 # Copies source files onto device into newly created folder
-Copy-Item -Path "$PSScriptRoot\npp.8.1.9.3.Installer.x64.exe" -Destination "C:\MDM\NP+\npp.8.1.9.3.Installer.x64.exe"
+Copy-Item -Path "$PSScriptRoot\npp.8.1.9.2.Installer.x64.exe" -Destination "C:\MDM\NP+\npp.8.1.9.2.Installer.x64.exe"
 
 # Uses PowerShell to run scripts contained in source folder
-start-process -FilePath "C:\MDM\NP+\npp.8.1.9.3.Installer.x64.exe" -ArgumentList '/S' -Verb runas -Wait
+start-process -FilePath "C:\MDM\NP+\npp.8.1.9.2.Installer.x64.exe" -ArgumentList '/S' -Verb runas -Wait

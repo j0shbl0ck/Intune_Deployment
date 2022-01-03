accfec45720d0b349929bc1e79f93bd632e3177b
Josh Block <247jblock@gmail.com>
2022-01-03 16:04:52 -0600

    Added batch file

---
+++ App Configurations/Notepad++/remove_files.bat
@@ -0,0 +1,12 @@
+:: Version 1.0.3
+:: This batch file excutes on startup to remove the folders and files below. In a sense, it removes the folder, then self distructs itself. 
+
+CONSOLESTATE /Hide 
+
+:: This removes the DeviceRename folder created.
+rmdir C:\MDM\DeviceRename /Q
+
+echo Y
+
+:: This removes the batch file copied into the Startup folder. 
+del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\remove_files.bat"

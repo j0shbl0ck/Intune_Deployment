3dc45c0becef2664182f9242e2ff515038033f64
Josh Block <247jblock@gmail.com>
2022-01-04 11:48:09 -0600

    Removed "echo Y" from Batch file

--- PowerShell scripts/Device Rename/remove_files.bat
+++ PowerShell scripts/Device Rename/remove_files.bat
@@ -6,7 +6,5 @@ CONSOLESTATE /Hide
 :: This removes the DeviceRename folder created.
 rmdir /s C:\MDM\DeviceRename /Q
 
-echo Y
-
 :: This removes the batch file copied into the Startup folder. 
 del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\remove_files.bat"

c8470b64913ebd7bec3fbd3f22a13e09bf1224da
Josh Block <247jblock@gmail.com>
2022-01-04 15:19:10 -0600

    Added "Uninstall.ps1"

---
+++ PowerShell scripts/Device Rename/uninstall.ps1
@@ -0,0 +1,16 @@
+<#
+    .NOTES
+    =============================================================================
+    Author: j0shbl0ck https://github.com/j0shbl0ck
+    Version: 1.0.1
+    Date: 12.21.21
+    Type: Public
+    Source: 
+    Description: Unistall SonicWallNetExtender without restart.
+    =============================================================================
+    .NOTES
+        "remove_files.bat" already removes the setup files needed. This is to provide Intune an uninstall command.
+#>
+
+
+

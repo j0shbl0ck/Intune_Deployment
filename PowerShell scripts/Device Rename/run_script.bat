316163e554a5a897be617278550cd9dd26ea6342
Josh Block <247jblock@gmail.com>
2022-01-05 12:00:23 -0600

    Removed "run_script.bat"

--- PowerShell scripts/Device Rename/run_script.bat
+++
@@ -1,6 +0,0 @@
-:: Version 1.0.3
-:: Batch file runs after inital Intune app deployment to run a rename PowerShell script. 
-
-CONSOLESTATE /Hide 
-:: This opens Command Prompt and runs "rename_device.ps1"
-powershell.exe -executionpolicy remotesigned -File "C:\MDM\DeviceRename\rename_device.ps1"

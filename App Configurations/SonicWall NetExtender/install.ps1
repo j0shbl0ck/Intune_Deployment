e7d9cb66a049a734cb80ffe1398d3935ee1e1149
Josh Block <247jblock@gmail.com>
2022-01-04 22:30:26 -0600

    Updated dashes. character U+2013 resembled U+002d

--- App Configurations/SonicWall NetExtender/install.ps1
+++ App Configurations/SonicWall NetExtender/install.ps1
@@ -2,7 +2,7 @@
     .NOTES
     =============================================================================
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.1
+    Version: 1.0.2
     Date: 12.21.21
     Type: Public
     Source: https://stackoverflow.com/questions/50274909/unattended-silent-install-notepad
@@ -11,11 +11,11 @@
 #>
 
 ##Copies files to the newly created folder "SonicWallNetExtender" located in C:\MDM
-New-Item –ItemType Directory –Force –Path "C:\MDM\SonicWallNetExtender" | Out-Null
-Copy-Item –Path "$PSScriptRoot\NetExtender-10.2.315.msi" –Destination "C:\MDM\SonicWallNetExtender\NetExtender-10.2.315.msi"
-Copy-Item –Path "$PSScriptRoot\profile.ps1" –Destination "C:\MDM\SonicWallNetExtender\profile.ps1"
+New-Item -ItemType Directory -Force -Path "C:\MDM\SonicWallNetExtender" | Out-Null
+Copy-Item -Path "$PSScriptRoot\NetExtender-10.2.315.msi" -Destination "C:\MDM\SonicWallNetExtender\NetExtender-10.2.315.msi"
+Copy-Item -Path "$PSScriptRoot\profile.ps1" -Destination "C:\MDM\SonicWallNetExtender\profile.ps1"
 ##Copies batch file to the startup folder to run on next startup of device
-Copy-Item –Path "$PSScriptRoot\Create_Profile.bat" –Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\Create_Profile.bat"
+Copy-Item -Path "$PSScriptRoot\Create_Profile.bat" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\Create_Profile.bat"
 
 #This installs SonicWall NetExtender. Note, this does a restart! If you perform /norestart the file will not install correct.
 msiexec /i "C:\MDM\SonicWallNetExtender\NetExtender-10.2.315.msi" /q 

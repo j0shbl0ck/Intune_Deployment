ee4381c49b02f173af8656a87242c880d537c09d
Josh Block <247jblock@gmail.com>
2022-01-06 14:34:34 -0600

    Updated script to check if NetExtender is installed

--- App Configurations/SonicWall NetExtender/install.ps1
+++ App Configurations/SonicWall NetExtender/install.ps1
@@ -2,7 +2,7 @@
     .NOTES
     =============================================================================
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.2
+    Version: 1.0.3
     Date: 12.21.21
     Type: Public
     Source: https://stackoverflow.com/questions/50274909/unattended-silent-install-notepad
@@ -10,15 +10,30 @@
     =============================================================================
 #>
 
-##Copies files to the newly created folder "SonicWallNetExtender" located in C:\MDM
+# Checks registry if SonicWall NetExtender is already installed
+$software = "{EF06A6A8-6B81-4A09-8223-789953972FFF}"
+$installed = (Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$software) 
+
+If($null -eq $installed) {
+# Creates new folder on C: Drive to host setup files
 New-Item -ItemType Directory -Force -Path "C:\MDM\SonicWallNetExtender" | Out-Null
+
+# Copies NetExtender.msi to newly created folder
 Copy-Item -Path "$PSScriptRoot\NetExtender-10.2.315.msi" -Destination "C:\MDM\SonicWallNetExtender\NetExtender-10.2.315.msi"
+
+#Copies profile.ps1 to newly created folder
 Copy-Item -Path "$PSScriptRoot\profile.ps1" -Destination "C:\MDM\SonicWallNetExtender\profile.ps1"
-##Copies batch file to the startup folder to run on next startup of device
+
+# Copies batch file to the startup folder to run on next startup of device
 Copy-Item -Path "$PSScriptRoot\Create_Profile.bat" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\Create_Profile.bat"
 
 #This installs SonicWall NetExtender. Note, this does a restart! If you perform /norestart the file will not install correct.
 msiexec /i "C:\MDM\SonicWallNetExtender\NetExtender-10.2.315.msi" /q 
+} else {
+    stop-process -ID $PID 
+}
+
+
 
 
 

a5c7e51d26c28dad13607bb1edfa53d3e54ba522
Josh Block <247jblock@gmail.com>
2022-01-07 17:00:44 -0600

    Updated uninstall.ps1

--- App Configurations/Google Chrome/uninstall.ps1
+++ App Configurations/Google Chrome/uninstall.ps1
@@ -1,1 +1,14 @@
-Start-Process 'C:\Windows\System32\msiexec.exe' "/X{B01A8859-9D45-3472-AD5D-0FB367564035} /qn /norestart" -Wait
+<#
+    .NOTES
+    =============================================================================
+    Author: j0shbl0ck https://github.com/j0shbl0ck
+    Version: 1.0.1
+    Date: 12.21.21
+    Type: Public
+    Source: 
+    Description: Unistall Google Chrome without restart.
+    =============================================================================
+#>
+
+#Removes Google Chrome by product code.
+Start-Process 'C:\Windows\System32\msiexec.exe' "/X{8093212B-7D0A-353D-8E41-3094713E73C5} /qn /norestart" -Wait

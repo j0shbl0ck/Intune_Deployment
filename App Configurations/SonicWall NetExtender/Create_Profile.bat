7c8974a2f482b902d24df1bed70dded49b1c8bfd
Josh Block <247jblock@gmail.com>
2022-01-07 14:10:28 -0600

    Removed "Consolestate /Hide"

--- App Configurations/SonicWall NetExtender/Create_Profile.bat
+++ App Configurations/SonicWall NetExtender/Create_Profile.bat
@@ -2,10 +2,7 @@
 :: This batch file first excutes the profile.ps1 script to add the profile within the SonicWall registry. 
     :: Afterwards, it closes the SonicWall service in order for it to properly appear when a user opens the application.
         :: Once the script is excuted and service is killed, the batch file self destructs as the profile is now added to the device. 
-
-
-CONSOLESTATE /Hide 
-
+:: =================================
 :: Executes powershell script
 Powershell.exe -executionpolicy remotesigned -File  C:\MDM\SonicWallNetExtender\profile.ps1
 

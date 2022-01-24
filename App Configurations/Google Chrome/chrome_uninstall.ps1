3dcec51f3858b2f58a23e610ce764dd8ac85edb9
Josh Block <247jblock@gmail.com>
2022-01-24 12:47:50 -0600

    Renamed file

--- App Configurations/Google Chrome/chrome_uninstall.ps1
+++ App Configurations/Google Chrome/chrome_uninstall.ps1
@@ -2,7 +2,7 @@
     .NOTES
     =============================================================================
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.1
+    Version: 1.0.2
     Date: 12.21.21
     Type: Public
     Source: 

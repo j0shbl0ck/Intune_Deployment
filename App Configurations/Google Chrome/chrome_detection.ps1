2386fae86c2ef05e94d6477db9878f9cef71eac9
Josh Block <247jblock@gmail.com>
2022-01-25 15:15:23 -0600

    Renamed file

--- App Configurations/Google Chrome/chrome_detection.ps1
+++ App Configurations/Google Chrome/chrome_detection.ps1
@@ -2,7 +2,7 @@
     .NOTES
     =============================================================================
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.4
+    Version: 1.0.5
     Date: 01.10.22
     Type: Public
     Source One: https://euc365.com/creating-intune-win32-apps/

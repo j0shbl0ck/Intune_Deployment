a700b87dd0f094e89672e07ed4149b9d9dd28f95
Josh Block <247jblock@gmail.com>
2022-01-25 11:11:33 -0600

    Added comment and removed exit code 0

--- App Configurations/Google Chrome/chrome_dectection.ps1
+++ App Configurations/Google Chrome/chrome_dectection.ps1
@@ -2,11 +2,12 @@
     .NOTES
     =============================================================================
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.1
+    Version: 1.0.2
     Date: 01.10.22
     Type: Public
     Source One: https://euc365.com/creating-intune-win32-apps/
     Source Two: https://techcommunity.microsoft.com/t5/microsoft-intune/intune-custom-detection-script/m-p/2976839
+    Source Three: https://universecitiz3n.tech/intune/Intune-Win32-CustomScript/
     Description: Used as custom detection script because of Chrome being possibly installed in two locations
     =============================================================================
     .README
@@ -20,7 +21,7 @@ $pathtwo = (Get-ChildItem "C:\Program Files (x86)\Google\Chrome\Application\chro
 
 IF ((Test-Path $pathone) -or (Test-Path $pathtwo)) {
     Write-Host "Google Chrome has been found"
-    Exit 0
+    #Exit code will be 0 and STDOUT not empty
 } else {
     Write-Host "Google Chrome has not been found"
     Exit 1

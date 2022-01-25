7297d2db43ffbce9945fa03dc11593018bca930c
Josh Block <247jblock@gmail.com>
2022-01-25 15:19:10 -0600

    Added additional source

--- App Configurations/Google Chrome/chrome_detection.ps1
+++ App Configurations/Google Chrome/chrome_detection.ps1
@@ -8,6 +8,7 @@
     Source One: https://euc365.com/creating-intune-win32-apps/
     Source Two: https://techcommunity.microsoft.com/t5/microsoft-intune/intune-custom-detection-script/m-p/2976839
     Source Three: https://universecitiz3n.tech/intune/Intune-Win32-CustomScript/
+    Source Four: https://social.technet.microsoft.com/Forums/en-US/c50de74f-4b68-4e90-ad19-0092aa5d61ac/how-to-set-exit-code-in-powershell-which-will-be-used-by-sccm?forum=configmanagergeneral 
     Description: Used as custom detection script because of Chrome being possibly installed in two locations
     =============================================================================
     .README

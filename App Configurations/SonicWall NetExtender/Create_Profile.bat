94ab8f51bc74a612d34f08454b64a62822c4e32a
Josh Block <247jblock@gmail.com>
2022-01-29 19:56:03 -0600

    Deleted All Files

--- App Configurations/SonicWall NetExtender/Create_Profile.bat
+++
@@ -1,8 +0,0 @@
-:: Version 1.0.1
-:: This batch file first excutes the profile.ps1 script to add the profile within the SonicWall registry. 
-:: =================================
-
-@echo off
-
-:: Executes powershell script
-Powershell.exe -Executionpolicy bypass -File  "C:\MDM\SonicWallNetExtender\profile.ps1"

dc03bf968e3d1476be1072749cc8893b686e5977
Josh Block <247jblock@gmail.com>
2022-01-31 10:44:05 -0600

    Updated uninstall file

--- App Configurations/Zoiper5/zoiper_uninstall.ps1
+++ App Configurations/Zoiper5/zoiper_uninstall.ps1
@@ -2,12 +2,15 @@
     .NOTES
     =============================================================================
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.5
-    Date: 01.10.22
+    Version: 1.0.50
+    Date: 01.31.22
     Type: Public
     Source: --
-    Description: Checks if Google Chrome is installed, if not, installs the application.
+    Description: Checks if Zoiper is installed, if not, installs the application.
     =============================================================================
     .README
     Please resort to README.md for additional file setup. 
 #>
+
+# Removes Notepad++ through uninstall.exe provided in source files. 
+start-process -FilePath "C:\Program Files (x86)\Zoiper5\Uninstall.exe" -ArgumentList '/S' -Verb runas -Wait

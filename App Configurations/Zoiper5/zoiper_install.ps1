66d63864ba21c08ca4edae647fe850fda8f3cbc4
Josh Block <247jblock@gmail.com>
2022-01-31 10:16:11 -0600

    Updated comments

--- App Configurations/Zoiper5/zoiper_install.ps1
+++ App Configurations/Zoiper5/zoiper_install.ps1
@@ -2,8 +2,8 @@
     .NOTES
     =============================================================================
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.5
-    Date: 01.10.22
+    Version: 1.0.0
+    Date: 01.31.22
     Type: Public
     Source: --
     Description: Checks if Google Chrome is installed, if not, installs the application.
@@ -11,3 +11,4 @@
     .README
     Please resort to README.md for additional file setup. 
 #>
+

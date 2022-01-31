1c9f3201eca4de4386df3180a56acbb414e617bd
Josh Block <247jblock@gmail.com>
2022-01-31 10:15:51 -0600

    Created Zoiper folder and files

---
+++ App Configurations/Zoiper5/zoiper_install.ps1
@@ -0,0 +1,13 @@
+<#
+    .NOTES
+    =============================================================================
+    Author: j0shbl0ck https://github.com/j0shbl0ck
+    Version: 1.0.5
+    Date: 01.10.22
+    Type: Public
+    Source: --
+    Description: Checks if Google Chrome is installed, if not, installs the application.
+    =============================================================================
+    .README
+    Please resort to README.md for additional file setup. 
+#>

dd7ef92b253547738f028ebb2dd52836caa15744
Josh Block <247jblock@gmail.com>
2022-03-30 20:34:14 -0500

    ⬆️  uploaded script

---
+++ PowerShell scripts/Change_Device_Ownership.ps1
@@ -0,0 +1,17 @@
+<#
+.SYNOPSIS
+    This script pulls from Microsoft Graph to change device ownership in Intune
+.DESCRIPTION
+    Author: j0shbl0ck https://github.com/j0shbl0ck
+    Version: 1.0.0
+    Date: 03.30.22
+    Type: Public
+.NOTES
+
+.LINK
+    Source: https://social.technet.microsoft.com/Forums/en-US/78f09120-5acf-4a0d-bb8d-fb9ed3973b00/intune-azure-portal-set-ownership-en-masse-via-powershell
+    Source: https://www.petervanderwoude.nl/post/change-device-ownership-the-journey-through-log-files-wmi-and-powershell-of-configmgr-2012/
+    Source: https://www.tftd.co.uk/2021/07/23/managing-intune-with-powershell/
+    Source: https://github.com/microsoftgraph/powershell-intune-samples/tree/master/ManagedDevices#1-invoke_deviceaction_setps1
+#>
+

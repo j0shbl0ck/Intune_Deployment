3e6695216db92a7ca17fede97793bb5b51b47fec
Josh Block <247jblock@gmail.com>
2022-04-07 23:44:38 -0500

    ⬆️  uploaded script

---
+++ Microsoft Win32 Content Prep Tool Setup/IntuneWinAppUtil.ps1
@@ -0,0 +1,17 @@
+﻿<#
+.SYNOPSIS
+    This script runs the IntuneWinAppUtil.exe from the C: drive. Please resort to the Github repo for more information.
+.DESCRIPTION
+    Author: j0shbl0ck https://github.com/j0shbl0ck
+    Version: 1.0.0
+    Date: 04.07.22
+    Type: Public
+.NOTES
+    Please see README.md for more information.
+
+.LINK
+
+#>
+
+Set-Location C:\IntuneWinAppUtil
+.\IntuneWinAppUtil.exe

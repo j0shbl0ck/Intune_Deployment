35454c47b8b9971ea7f8693f64885ffe8d6931db
Josh Block <247jblock@gmail.com>
2022-04-21 18:35:00 -0500

    🔨  editing bat file

--- App Configurations/M365 Shortcuts/Createlnk.bat
+++ App Configurations/M365 Shortcuts/Createlnk.bat
@@ -1,7 +1,7 @@
-:: Version 1.0.0
+:: Version 1.0.1
 :: Batch file runs on startup to execult the provided PowerShell script below. 
 
 CONSOLESTATE /Hide 
 
 :: Runs PowerShell script to create shortcut icons
-Powershell.exe -executionpolicy remotesigned -File  C:\MDM\Createlnk\Createlnk.ps1
+Powershell.exe -Command "& {Start-Process Powershell.exe -ArgumentList '-ExecutionPolicy Bypass -File C:\MDM\Createlnk\Createlnk.ps1' -Verb RunAs}"

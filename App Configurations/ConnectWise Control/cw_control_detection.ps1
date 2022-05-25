2dfe27ee901a857f20f93d847aaa9ae773c8d917
Josh Block <247jblock@gmail.com>
2022-05-25 13:31:43 -0500

    ✏️  created and uploaded detection script

---
+++ App Configurations/ConnectWise Control/cw_control_detection.ps1
@@ -0,0 +1,33 @@
+<#
+.SYNOPSIS
+    This script checks if ConnectWise Control was installed on the device.
+    Author: Josh Block
+.NOTES
+    Version: 1.0.0
+    Date: 05.25.22
+    Type: Public
+.LINK
+    https://github.com/j0shbl0ck
+    https://docs.microsoft.com/en-us/mem/intune/apps/apps-win32-troubleshoot#detecting-the-win32-app-file-version-by-using-powershell
+#>
+
+# File path to AARC 64bit location
+$pathone = Test-path "${env:ProgramFiles}\ScreenConnect Client*"
+
+# File path to AARC 64bit location
+$pathtwo = Test-path "${env:ProgramFiles(x86)}\ScreenConnect Client*"
+
+# Get's host name of device
+$hostname = hostname
+
+if(("True" -eq $pathone) -or ("True" -eq $pathtwo))
+{
+    # Write out a random variable (in this case hostname) to output STDOUT
+    $hostname
+    Exit 0
+}
+else
+{
+    # Exit with non=zero failure code
+    Exit 1
+}

c6b92c527fd685f150b249bdafc9a6a921b74878
Josh Block <247jblock@gmail.com>
2022-06-04 22:53:30 -0500

    🔨  fixed comment line

--- App Configurations/Adobe Acrobat Reader DC/AReaderDC_detection.ps1
+++ App Configurations/Adobe Acrobat Reader DC/AReaderDC_detection.ps1
@@ -2,7 +2,7 @@
     .NOTES
     =============================================================================
     Author: j0shbl0ck https://github.com/j0shbl0ck
-    Version: 1.0.0
+    Version: 1.0.1
     Date: 02.17.22
     Type: Public
     Source: https://docs.microsoft.com/en-us/mem/intune/apps/apps-win32-troubleshoot#detecting-the-win32-app-file-version-by-using-powershell
@@ -15,7 +15,7 @@
 # File path to AARC 64bit location
 $pathone = Test-path "${env:ProgramFiles}\Adobe\Acrobat DC\Acrobat\Acrobat.exe"
 
-# File path to AARC 64bit location
+# File path to AARC 32bit location
 $pathtwo = Test-path "${env:ProgramFiles(x86)}\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"
 
 # Get's host name of device

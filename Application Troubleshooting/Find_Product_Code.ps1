ba126dcd352f972ae306fdff5b0a04023ece2091
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-08-04 18:25:44 -0500

    🔨  Added Clear-Host to script

--- Application Troubleshooting/Find_Product_Code.ps1
+++ Application Troubleshooting/Find_Product_Code.ps1
@@ -3,13 +3,18 @@
     This fetches the product ID of applications for removing products via MSI if available. 
     Author: Josh Block
 .NOTES
-    Version: 1.0.2
+    Version: 1.0.3
     Date: 05.25.22
     Type: Public
 .LINK
     https://github.com/j0shbl0ck
 #>
 
+# clear the screen
+Clear-Host
+
+# Retrive all the products installed on the system
 get-wmiobject Win32_Product | Sort-Object -Property Name | Format-Table IdentifyingNumber, Name, LocalPackage -AutoSize
 
+# Allow user to view the products installed on the system
 Pause

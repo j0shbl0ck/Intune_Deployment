d07354331c5d5ed567aab1a07fba40f0e7649d87
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-10-05 10:25:46 -0500

    ⚒️  updated command line to install Dell Command Update

--- App Configurations/Dell Command Update/dellcommand_install.ps1
+++ App Configurations/Dell Command Update/dellcommand_install.ps1
@@ -3,7 +3,7 @@
     Installs the latest version of Dell Command Update
     Author: Josh Block
 .NOTES
-    Version: 1.0.1
+    Version: 1.0.2
     Date: 05.03.22
     Type: Public
 .LINK
@@ -19,7 +19,7 @@ New-Item -ItemType Directory -Force -Path "C:\MDM\DellCommand" | Out-Null
 Copy-Item -Path "$PSScriptRoot\Dell-Command-Update-Application_8D5MC_WIN_4.3.0_A00_02.EXE" -Destination "C:\MDM\Dell-Command-Update-Application_8D5MC_WIN_4.3.0_A00_02.EXE"
 
 #This installs Dell Command Update. Note, you will need to change version.
-Start-Process -FilePath "C:\MDM\Dell-Command-Update-Application_8D5MC_WIN_4.3.0_A00_02.EXE" -ArgumentList "/S /v/qn"
+Start-Process -FilePath "C:\MDM\Dell-Command-Update-Application_8D5MC_WIN_4.3.0_A00_02.EXE" -ArgumentList "/S"
 
 # Wait for the installation of Dell Command Update to deploy. 
 Start-Sleep -s 60

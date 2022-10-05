1989f6acb77135555b39161aa016ba4c1de49e22
Josh Block <70075839+j0shbl0ck@users.noreply.github.com>
2022-10-05 11:02:45 -0500

    ⚒️  has script verify if DCU is already installed

--- App Configurations/Dell Command Update/dellcommand_install.ps1
+++ App Configurations/Dell Command Update/dellcommand_install.ps1
@@ -12,14 +12,28 @@
     https://scloud.work/en/dell-driver-intune/
 #>
 
-# Creates new folder on C: Drive to host setup files
-New-Item -ItemType Directory -Force -Path "C:\MDM\DellCommand" | Out-Null
+# Checks File Explorer if Dell Command Update is present on device
+$pathone = (Get-ChildItem "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe")
 
-# Copies Dell Command Update setup file into newly created folder. Note, you will need to change version. 
-Copy-Item -Path "$PSScriptRoot\Dell-Command-Update-Application_8D5MC_WIN_4.3.0_A00_02.EXE" -Destination "C:\MDM\Dell-Command-Update-Application_8D5MC_WIN_4.3.0_A00_02.EXE"
+If($null -eq $pathone) {
 
-#This installs Dell Command Update. Note, you will need to change version.
-Start-Process -FilePath "C:\MDM\Dell-Command-Update-Application_8D5MC_WIN_4.3.0_A00_02.EXE" -ArgumentList "/S"
+    # ======== CREATE NEW FOLDER AND COPY FILES ========
 
-# Wait for the installation of Dell Command Update to deploy. 
-Start-Sleep -s 60
+    # Creates new folder on C: Drive to host setup files
+    New-Item -ItemType Directory -Force -Path "C:\MDM\DellCommand" | Out-Null
+
+    # Copies Dell Command Update setup file into newly created folder. Note, you will need to change version. 
+    Copy-Item -Path "$PSScriptRoot\Dell-Command-Update-Application_8D5MC_WIN_4.3.0_A00_02.EXE" -Destination "C:\MDM\Dell-Command-Update-Application_8D5MC_WIN_4.3.0_A00_02.EXE"
+
+    # ======== INSTALL DELL COMMAND UPDATE ========
+
+    #This installs Dell Command Update. Note, you will need to change version.
+    Start-Process -FilePath "C:\MDM\Dell-Command-Update-Application_8D5MC_WIN_4.3.0_A00_02.EXE" -ArgumentList "/S"
+
+    # Wait for the installation of Dell Command Update to deploy. 
+    Start-Sleep -s 60
+
+} else {
+    # This shuts down powershell, if application is already installed.
+    stop-process -ID $PID 
+}

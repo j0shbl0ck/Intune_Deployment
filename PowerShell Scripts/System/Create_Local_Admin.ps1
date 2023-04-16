ac8f956aacab7ff3887a07af70971a56edbca546
Josh Block <0go1fbn9c@relay.firefox.com>
2023-04-16 18:04:03 -0500

    🦄  script not working

--- PowerShell Scripts/System/Create_Local_Admin.ps1
+++ PowerShell Scripts/System/Create_Local_Admin.ps1
@@ -3,7 +3,7 @@
     This script creates a localadmin account on the device. Use .\ to login into system.
     Author: Josh Block
 .NOTES
-    Version: 1.0.5
+    Version: 1.0.6
     Date: 12.21.21
     Type: Public
     =============================================================================
@@ -25,37 +25,48 @@
 ## For new PS scripters, leave the qoutes, but replace <text>.
 ## Run this script in PowerShell ISE as admin to properly edit the script.
 
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    # If not elevated, display an error message and exit the script after a 10-second delay
+    Write-Error "This script must be run as an administrator."
+    Start-Sleep -Seconds 10
+    Exit
+}
+
+#At this point, the script is running in an elevated PowerShell session
 Clear-Host
 
-$ExpectedLocalUser = "<username>"
-$Password = ConvertTo-SecureString '<password>' -AsPlainText -Force
+$ExpectedLocalUser = Read-Host "Enter the desired local username"
+$Password = Read-Host "Enter the password"
+$Password2 = Read-Host "Enter the password again"
 
-Function Create_LocalAdmin
-{
+if ($Password -eq $Password2) {
+    $SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+    
+    Function Create_LocalAdmin
+    {
+        $FullName = Read-Host "Enter the user's full name"
+        New-LocalUser $ExpectedLocalUser -Password $SecurePassword -FullName $FullName -Description "Local Administrator account."
+        Add-LocalGroupMember -Group "Administrators" -Member $ExpectedLocalUser
+        Set-LocalUser -Name $ExpectedLocalUser -PasswordNeverExpires:$true
+    }
 
-    New-LocalUser $ExpectedLocalUser -Password $Password -FullName "<First Last>" -Description "Local Administrator account."
-    Add-LocalGroupMember -Group "Administrators" -Member $ExpectedLocalUser
-    Set-LocalUser -Name $ExpectedLocalUser -PasswordNeverExpires:$true
+    Try
+    {
+        Write-Host "User $ExpectedLocalUser already exists. Updating password..."
+        ## If an account is found update the password
+        Set-LocalUser -Name $ExpectedLocalUser -Password $SecurePassword -PasswordNeverExpires:$true
+    }
+    Catch
+    {
+        Write-Host "User $ExpectedLocalUser does not exist. Creating account..."
+        Create_LocalAdmin
+    }
+}
+else {
+    Write-Error "Passwords do not match. Please try again."
+    Start-Sleep -Seconds 5
+    Exit
 }
 
-Try
-
-{
-
-    ## Catch if not found
-    $LocaAdminUser = Get-LocalUser -Name $ExpectedLocalUser -ErrorAction Stop 
-
-    ## If an account is found update the password
-    Set-LocalUser -Name $ExpectedLocalUser -Password $Password -PasswordNeverExpires:$true
-
-}
-
-Catch
-
-{
-
-    Create_LocalAdmin
-
-}
 
 

fb5d51f03c0cd8c290ab17d6c6a875499bcc5af6
Josh Block <0go1fbn9c@relay.firefox.com>
2023-08-09 16:49:01 -0500

    🐛  added additional prompts for the script

--- PowerShell Scripts/System/windowsUpdates.ps1
+++ PowerShell Scripts/System/windowsUpdates.ps1
@@ -3,7 +3,7 @@
     This script updates the Windows system and reboots if needed.
     Author: Josh Block
 .NOTES
-    Version: 1.0.0
+    Version: 1.0.1
     Date: 08.08.23
     Type: Public
 .LINK
@@ -11,8 +11,17 @@
     https://pureinfotech.com/install-windows-10-update-powershell/
 #>
 
+# Set the execution policy to bypass
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+# Install package provider NuGet
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
 # Install the module to run Windows Update
 Install-Module PSWindowsUpdate -Force
+
+# Import the module to run Windows Update
+Import-Module PSWindowsUpdate
 
 # Check updates with PowerShell
 Get-WindowsUpdate
